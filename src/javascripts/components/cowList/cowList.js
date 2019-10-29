573674232a26cf12f6efaebaf6ef986554fabe24
Gabriel Seals <sealsgj@gmail.com>
2019-10-28 19:14:17 -0500

    made getCows a promise

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -4,13 +4,7 @@ import './cowList.scss';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        demCows[cowId].id = cowId;
-        cows.push(demCows[cowId]);
-      });
-      console.log('it worked!', cows);
+      console.log('cow array from cowList', response);
     })
     .catch((error) => {
       console.error('shit broke', error);
--- src/javascripts/helpers/data/cows.js
+++ src/javascripts/helpers/data/cows.js
@@ -3,7 +3,16 @@ import axios from 'axios';
 
 // const getCows = () => axios.get('https://teamtreehouse.com/ashleyclaiborne.json');
 
-const getCows = () => axios.get('../../../../db/cows.json');
-
+const getCows = () => new Promise((resolve, reject) => {
+  axios.get('../../../../db/cows.json').then((response) => {
+    const demCows = response.data.cows;
+    const cows = [];
+    Object.keys(demCows).forEach((cowId) => {
+      demCows[cowId].id = cowId;
+      cows.push(demCows[cowId]);
+    });
+    resolve(cows);
+  }).catch((error) => reject(error));
+});
 
 export default { getCows };
